7882cadba3c551c6c69220ed0c8f70e6f4a1984c
Alexandre Ratte <alex@afelox.com>
2018-12-01 09:54:00 -0500

    20181201 part 1 completed.

--- 20181201/Program.cs
+++ 20181201/Program.cs
@@ -10,6 +10,14 @@ namespace _20181201
     {
         static void Main(string[] args)
         {
+            var frequencyModulations = System.IO.File.ReadAllLines(@"T:\SVN\adventofcode.com_2018\20181201\input.txt");
+            int frequency = 0;
+
+            foreach (var modulation in frequencyModulations)
+                frequency += Int32.Parse(modulation);
+
+            Console.WriteLine("Resulting frequency: {0}", frequency);
+            Console.ReadKey();
         }
     }
 }
