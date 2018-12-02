c798e32a25e604e140dffa8110c5bdff30aadc80
Alexandre Ratte <alex@afelox.com>
2018-12-02 09:14:16 -0500

    Both parts of 20181202 completed.

---
+++ 20181202_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20181202_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var boxIDs = System.IO.File.ReadAllLines(@"T:\SVN\adventofcode.com_2018\20181202_1\input.txt");
+            int twice = 0, thrice = 0;
+            char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            foreach (string ID in boxIDs)
+            {
+                bool twiceFound = false, thriceFound = false;
+                foreach (char letter in letters)
+                {
+                    if (ID.Where(c => c == letter).Count() == 2 && !twiceFound)
+                    {
+                        twice += 1;
+                        twiceFound = true;
+                    }
+                    if (ID.Where(c => c == letter).Count() == 3 && !thriceFound)
+                    {
+                        thrice += 1;
+                        thriceFound = true;
+                    }
+                    if (twiceFound && thriceFound)
+                        break;
+                }
+            }
+
+            Console.WriteLine("Solution is: {0}", twice * thrice);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 20181202_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20181202_2
+{
+    class Program
+    {
+        static string GetMatchingBoxes(string first, string second)
+        {
+            int count = 0;
+            int position = int.MinValue;
+            for (int i=0;i<first.Length; i++)
+                if (first[i] != second[i])
+                {
+                    count += 1;
+                    position = i;
+                }
+
+            if (count != 1)
+                return null;
+            else
+                return first.Substring(0,position) + first.Substring(position+1);
+        }
+
+        static void Main(string[] args)
+        {
+            var boxIDs = System.IO.File.ReadAllLines(@"T:\SVN\adventofcode.com_2018\20181202_1\input.txt");
+            foreach (string firstID in boxIDs)
+                foreach (string secondID in boxIDs.Where(b => b != firstID))
+                {
+                    string solution = GetMatchingBoxes(firstID, secondID);
+                    if (solution != null)
+                    {
+                        Console.WriteLine("Solution is: {0}", solution);
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+        }
+    }
+}
